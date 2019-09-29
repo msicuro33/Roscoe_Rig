f6c8d99e4b66c1521e2639c652b5311808e6f62d
Msicuro <35858231+Msicuro@users.noreply.github.com>
2019-09-29 18:45:58 -0400

    Updated comments in ctrl curves script and saved maya files

--- Scripts/create_control_curves.py
+++ Scripts/create_control_curves.py
@@ -1,14 +1,18 @@
-'''
-Place to store functions to create control curves
+#########################################################
+### Place to store functions to create control curves ###
+#########################################################
+
 
 '''
-
-#Create partial box for tail controls
-###May need to add snapping ivot point to center###
+Create partial box for tail controls
+'''
+###May need to add snapping pivot point to center###
 def create_tail_control_box(tail_ctrl_name):
 	cmds.curve(n = tail_ctrl_name, d = 1, p = [(-1, 2, 0), (-1, 0, 0), (1, 0, 0), (1, 2, 0), (1, 2, -2), (-1, 2, -2), (-1, 2, 0), (-1, 0, 0), (-1, 0, -2), (1, 0, -2), (1, 0, 0), (1, 2, 0), (-1, 2, 0)], k = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
-#Create full box
+'''
+Create full box
+'''
 ###May need to add snapping ivot point to center###
 def createBox(ctrl_name):
 	cmds.curve(n = ctrl_name, d = 1, p = [(-1, 2, 0), (-1, 0, 0), (1, 0, 0), (1, 2, 0), (1, 2, -2), (1, 0, -2), (-1, 0, -2), (-1, 2, -2), (-1, 2, 0), (-1, 0, 0), (-1, 0, -2), (-1, 2, -2), (1, 2, -2), (1, 0, -2), (1, 0, 0), (1, 2, 0), (-1, 2, 0)], k = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
