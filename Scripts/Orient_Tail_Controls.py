8bfdf9e16342cfbcef4ded090fc9ec1bbb219f4f
Msicuro <35858231+Msicuro@users.noreply.github.com>
2019-12-01 20:56:22 -0500

    Fixed small bug with translates and rotates variable names being switched

--- Scripts/Orient_Tail_Controls.py
+++ Scripts/Orient_Tail_Controls.py
@@ -10,8 +10,8 @@ def orientControls():
 	current = cmds.ls(sl=True)
 
 #Query the Rotate and Translate values of the first of two current selections and store in variables
-	rotates = cmds.xform('current[0]',q=True, t = True, ws = True)
-	translates = cmds.xform('current[0]',q=True, ro = True, ws = True)
+	translates = cmds.xform('current[0]',q=True, t = True, ws = True)
+	rotates = cmds.xform('current[0]',q=True, ro = True, ws = True)
 #Move the second selection with the Translate and Rotate values of the first
 	cmds.xform('current[1]', t = translates, ws = True)
 	cmds.xform('current[1]', ro = rotates, ws = True)
